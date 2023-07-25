45178d2cf62767b567493b86e4ed0fea1e571565
satoshi-sh <satoss1108@gmail.com>
2023-07-25 16:33:22 -0500

    Changed port for the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const Member = require("./models/member");
 const Project = require("./models/project");
 const Image = require("./models/image");
