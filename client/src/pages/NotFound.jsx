6a8b9e04b6f59a1c45c141a9e5a80666f9c51fcb
satoshi-sh <satoss1108@gmail.com>
2023-08-30 09:43:12 -0500

    Removed a unessesary paragraph.

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -5,7 +5,6 @@ const NotFound = () => {
     <div className="pt-32 text-center">
       <h1 className="text-3xl">404 - Page Not Found</h1>
       <p className="mt-4">The page you are looking for does not exist.</p>
-      <p>Project Owner might delete the project</p>
     </div>
   );
 };
